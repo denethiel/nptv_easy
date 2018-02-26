d1a0eb63a6465a7e8aecbdbfdde920a71e120b40
NeoPolitica TV <neopoliticatv@iMac-de-NeoPolitica.local>
2018-02-26 17:48:56 -0600

    add menu

--- includes/class-nptv-easy.php
+++ includes/class-nptv-easy.php
@@ -7,31 +7,90 @@ class Nptv_Easy {
 
     private $capability = 'manage_options' ;
 
-    public function __construct($plugin_meta = array() ){
-        if(empty($plugin_meta)){
+    public function __construct(){
+        
+        // if(empty($plugin_meta)){
+        //     return;
+        // }
+
+        //var_dump(current_user_can($this->capability));
+
+        // $this->plugin_meta = $plugin_meta;
+
+        // El uso de este plugin esta reservado para Administradores
+
+        if( ! current_user_can( $this->capability )) {
+            //wp_die('No tienes suficientes provlegios para editar esta pagina');
             return;
         }
 
-        $this->plugin_meta = $plugin_meta;
+        add_action('init', array($this, 'init'));
 
-        // El uso de este plugin esta reservado para Administradores
-        if(! current_user_can( $capability )) {
-            return ;
-        }
+    }
+
+    function init(){
+
 
 
         // Agregar pagina del Plugin dentro del Setup
 
-        add_action('admin_menu', array( $this, 'nptv_easy_menu'));
-    }
-
-    public function nptv_easy_menu(){
-        add_options_page( $this->plugin_meta['name'] . 'Plugin', $this->plugin_meta['name'] , $capability, $this->plugin_meta['slug'], array($this, 'nptv_control_options_page') );
-    }
-
-    public function nptv_control_options_page(){
         
+        add_action('admin_menu', array( $this, 'nptv_build_menu'));
+
+
+        //Agrega el JS Script
+        add_action('admin_enqueue_scripts', array( $this, 'enqueue_scripts'));
+    }
+
+
+    
+
+    public function nptv_build_menu(){
+
+        /*
+        Usamos la funcion add_options_page
+        add_options_page ( $page_title, $menu_title, $capability, $menu-slug, $funcion)
+        */
+
+
+        add_menu_page( 
+            NPTV_NAME, 
+            NPTV_NAME, 
+            $this->capability, 
+            NPTV_SLUG, 
+            array($this,'nptv_easy_options_page'), 
+            plugins_url( 'nptv-easy/src/assets/logo_menu.jpg', NPTV_DIR), 
+            6);
+
+        // add_options_page(
+        //     $this->plugin_meta['name'] . 'Plugin', 
+        //     $this->plugin_meta['name'] , 
+        //     'manage_options', 
+        //     $this->plugin_meta['slug'], 
+        //     array( $this, 'nptv_easy_options_page') 
+        // );
+    }
+
+    public function nptv_easy_options_page(){
+
+        echo "Estoy en el constructor del menu";
+
+        if(!current_user_can($this->capability)){
+            wp_die('No tienes suficientes provilegios para editar esta pagiba - NPTV Easy');
+        }
+        ?>
+        <!-- Main content -->
+        <div id="nptv-easy-main">
+            
+        </div>
+        <?php
+    }
+
+    public function enqueue_scripts($hook_page){
+        echo $hook_page;
     }
 }
+
+new Nptv_Easy();
 
 ?>
--- nptv-easy.php
+++ nptv-easy.php
@@ -1,19 +1,82 @@
 <?php 
 /**
- * @link              https://github.com/ianmjones/wp-cron-pixie
- * @package           Cron_Pixie
+ * @link              https://github.com/denethiel/nptv-easy
+ * @package           NPTV Easy
  *
  * @wordpress-plugin
  * Plugin Name:       NPTV Easy
- * Plugin URI:        https://github.com/ianmjones/wp-cron-pixie
- * Description:       A little dashboard widget to manage the WordPress cron.
- * Version:           1.1
- * Author:            Ian M. Jones
- * Author URI:        http://www.ianmjones.com/
+ * Plugin URI:        https://github.com/denethiel/nptv-easy
+ * Description:       Un Plugin para hacer mas facil todo.
+ * Version:           0.3 Alpha 
+ * Author:            Jose David Pacheco Valedo
+ * Author URI:        http://www.twitter.com/josepdvahn
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wp-cron-pixie
+ * Text Domain:       nptv-easy
  * Domain Path:       /languages
  * Network:           False
  */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+include_once(ABSPATH . 'wp-includes/pluggable.php');
+
+class NeoPoliticaTV_Easy 
+{
+	// private plugin_meta = array(
+	// 	'slug'    => 'nptv-easy',
+	// 	'name'    => 'NPTV Easy',
+	// 	'file'    => __FILE__,
+	// 	'version' => '0.3',
+	// )
+
+	private static $instance;
+
+	function __construct(){
+		define('NPTV_URL', plugins_url('', __FILE__));
+		define('NPTV_DIR', dirname(__FILE__));
+		define('NPTV_NAME', 'NPTV Easy');
+		define('NPTV_SLUG', 'nptv-easy');
+		define('NPTV_VERSION','0.3');
+
+		include ( NPTV_DIR . '/includes/class-nptv-easy.php');
+	}
+
+	public static function instance(){
+		if(! isset(self::$instance)){
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+
+}
+
+function NPTV() {
+	return NeoPoliticaTV_Easy::instance();
+}
+
+$nptv_easy = NPTV();
+
+
+// // Donde la magia sucede...
+// require plugin_dir_path( __FILE__ ) . 'includes/class-nptv-easy.php';
+
+// /**
+//  * Inicia el plugin en el hook correcto
+//  */
+
+// function nptv_easy_admin_init(){
+// 	if(is_admin()){
+// 	$nptv_easy = new Nptv_Easy( nptv_easy_meta() );
+// 	}
+// }
+
+
+
+// add_action('admin_init','nptv_easy_admin_init');
+
  ?>
